ad222a9cba9402ca10ba88220b6fc5fed5a953e5
JBEDU12 <JBEDU12@DESKTOP-47LS4DD>
2022-03-22 09:46:06 +0900

    ha ha ha 22.03.22 - 02

---
+++ src/git_test_02/Sibal.java
@@ -0,0 +1,10 @@
+package git_test_02;
+
+public class Sibal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("please have a head");
+	}
+
+}
